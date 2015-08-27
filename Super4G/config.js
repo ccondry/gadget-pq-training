91f4fbfc14796178c4cbc3c057409dcc219cf4b5
Coty Condry <coty@spinsci.com>
2015-08-27 10:10:26 -0500

    Organize files, remove unused files, move html content out of xml file and into its own file, move config variables into their own config.js file, and updated config for UCCE lab

---
+++ Super4G/config.js
@@ -0,0 +1,4 @@
+var UCCE_DS_IP = "prgr-a.cis.cisco.com";
+var UCCE_SIDE_B = "prgr-b.cis.cisco.com";
+var authString = "Y2NlYWRtaW5AY2lzLmNpc2NvLmNvbTpDaXNjbzEyM2Npc2Nv";
+var quizAttributeRefID = "5005";
